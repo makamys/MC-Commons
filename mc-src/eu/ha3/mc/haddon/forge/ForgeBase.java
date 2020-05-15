f4fce8a46eb7d62a43e2a2e7f222769b05b27e59
makamys <makamys@outlook.com>
2020-05-16 00:19:13 +0200

    Forwardport 1.7.10 branch
    Cherry-pick of the following commits:
    
    8eac73e1809af085a0b559610484680512fd8fbb Make it possible to add descriptions to config properties
    572394315e37ea9de100b7646b2546eadc600934 Add SupportsBlockChangeEvents (only implemented in Forge)
    9629c0bf518068896028d13e17eeaa7d30410ca8 Add global descriptions to config files
    af8f99c135a30f8fea8f7b05c229a188ab3eb99b Fix build
    d257d5b8783121ac3ea4770f5ef12c3fd2900b62 Handle block change events on client side
    76ffb8966d36155358d1964871c6eedb91db2ed2 Add SupportsSoundEvents (only implemented in Forge)

--- mc-src/eu/ha3/mc/haddon/forge/ForgeBase.java
+++ mc-src/eu/ha3/mc/haddon/forge/ForgeBase.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.profiler.Profiler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -15,8 +16,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Timer;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.event.world.BlockEvent.PlaceEvent;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.logging.log4j.Logger;
 
@@ -26,9 +38,13 @@ import eu.ha3.mc.haddon.OperatorCaster;
 import eu.ha3.mc.haddon.implem.HaddonUtilityImpl;
 import eu.ha3.mc.haddon.implem.ProfilerHelper;
 import eu.ha3.mc.haddon.supporting.SupportsInGameChangeEvents;
+import eu.ha3.mc.haddon.supporting.SupportsBlockChangeEvents;
 import eu.ha3.mc.haddon.supporting.SupportsFrameEvents;
 import eu.ha3.mc.haddon.supporting.SupportsPlayerFrameEvents;
+import eu.ha3.mc.haddon.supporting.SupportsSoundEvents;
 import eu.ha3.mc.haddon.supporting.SupportsTickEvents;
+import eu.ha3.mc.haddon.supporting.event.BlockChangeEvent;
+import eu.ha3.mc.haddon.supporting.SupportsBlockChangeEvents.ClickType;
 
 
 public class ForgeBase implements OperatorCaster
@@ -41,6 +57,8 @@ public class ForgeBase implements OperatorCaster
     protected final boolean suFrame;
     protected final boolean suFrameP;
     protected final boolean suInGame;
+    protected final boolean suSound;
+    protected final boolean suBlockChange;
     
     protected int tickCounter;
     protected boolean enableTick;
@@ -49,12 +67,16 @@ public class ForgeBase implements OperatorCaster
     private int ticksSinceLastRender = 0;
     private boolean wasInGame;
     
+    private Queue<BlockChangeEvent> blockEventQueue = new LinkedBlockingQueue<>();
+    
     public ForgeBase(Haddon haddon) {
         this.haddon = haddon;
         suTick = haddon instanceof SupportsTickEvents;
         suFrame = haddon instanceof SupportsFrameEvents;
         suFrameP = haddon instanceof SupportsPlayerFrameEvents;
         suInGame = haddon instanceof SupportsInGameChangeEvents;
+        suBlockChange = haddon instanceof SupportsBlockChangeEvents;
+        suSound = haddon instanceof SupportsSoundEvents;
         
         shouldTick = suTick || suFrame;
         
@@ -94,6 +116,17 @@ public class ForgeBase implements OperatorCaster
         onTickLiteLoaderStyle(mc, partialTicks, inGame, clock);
     }
     
+    @SubscribeEvent
+    public void onSoundEvent(PlaySoundEvent event) {
+        if (!shouldTick) return;
+        
+        if(suSound) {
+            if(!((SupportsSoundEvents)haddon).onSound(event.getSound(), event.getName(), event.getManager())) {
+                event.setResultSound(null);
+            }
+        }
+    }
+    
     private void onTickLiteLoaderStyle(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) {
         if(inGame != wasInGame) {
             if(suInGame) {
@@ -114,6 +147,14 @@ public class ForgeBase implements OperatorCaster
             }
             tickCounter++;
         }
+        
+        if(suBlockChange) {
+            while(!blockEventQueue.isEmpty()) {
+                ((SupportsBlockChangeEvents)haddon).onBlockChanged(blockEventQueue.remove());
+            }
+            
+        }
+        
         if (enableFrame) {
             if (suFrame) {
                 ((SupportsFrameEvents)haddon).onFrame(partialTicks);
@@ -135,6 +176,27 @@ public class ForgeBase implements OperatorCaster
         MinecraftForge.EVENT_BUS.register(this);
         
         haddon.onLoad();
+    }
+    
+    @SubscribeEvent
+    public void onBlock(BlockEvent event) {
+        if (!shouldTick) return;
+        
+        if(suBlockChange) {
+            Block oldBlock = null, newBlock = null;
+            if(event instanceof PlaceEvent) {
+                oldBlock = Block.getBlockById(0);
+                newBlock = ((PlaceEvent) event).getPlacedBlock().getBlock();
+            } else if(event instanceof BreakEvent) {
+                oldBlock = event.getState().getBlock();
+                newBlock = Minecraft.getMinecraft().world.getBlockState(event.getPos()).getBlock();
+            }
+            if(oldBlock != null) {
+                // this is on server side! we must send it to client side and process it there
+                blockEventQueue.add(new BlockChangeEvent(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(),
+                        oldBlock, newBlock));
+            }
+        }
     }
 
     @Override
---
+++ mc-src/eu/ha3/mc/haddon/supporting/SupportsSoundEvents.java
@@ -0,0 +1,10 @@
+package eu.ha3.mc.haddon.supporting;
+
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.SoundManager;
+
+public interface SupportsSoundEvents {
+    
+    public boolean onSound(ISound sound, String name, SoundManager manager);
+    
+}
